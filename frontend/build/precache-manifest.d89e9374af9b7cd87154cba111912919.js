5b6c7889da1c2054b46eefcca9ec5f317259351b
Krzysztof Wielicki <kwielicki@e-point.pl>
2021-03-08 11:24:49 +0100

    FISHBOOK-003 Enjecting create-react-app && changing project structore for BE/FE

---
+++ frontend/build/precache-manifest.d89e9374af9b7cd87154cba111912919.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ed20b2853ab465b07654fa0e0bb0a6d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "25d70338a099b63821d9",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "eabdf1b2541aab828f8e",
+    "url": "/static/js/2.5722500a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5722500a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25d70338a099b63821d9",
+    "url": "/static/js/main.abb91cfc.chunk.js"
+  },
+  {
+    "revision": "ad2fd2475d4adc05bae3",
+    "url": "/static/js/runtime-main.08d2193b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import { Button } from '@material-ui/core';
 function App() {
   return (
     <div className="App">
@@ -10,14 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React.
-        </a>
+        <Button variant='contained'>Button</Button>
       </header>
     </div>
   );
